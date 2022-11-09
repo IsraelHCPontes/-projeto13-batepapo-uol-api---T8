a1c1c45cc5373428d793cdc93060b9f104c878ed
israel henrique castro pontes <israelpontes@MacBook-Pro-de-israel.local>
2022-11-09 00:11:59 -0300

    feat(dotenv)Configurei a biblioteca dotenv e a variavel de ambiete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 //configs
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
 
+mongoClient.connect().then(() => {
+    db = mongoClient.db("batePapoUol");
+})
 
 
 app.listen(5001, () => console.log('Ouvindo na porta 5001'))
