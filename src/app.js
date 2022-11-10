33e95b879e508202078d3639654c7dc6a60a41ef
israel henrique castro pontes <israelpontes@MacBook-Pro-de-israel.local>
2022-11-09 22:16:19 -0300

    feat(POST/participants)Implementei a rota que recebe e catolga usuario na collection users e uma mensagem na callectionmessages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
+import dayjs from 'dayjs';
 //configs
 const app = express();
 app.use(cors());
@@ -13,6 +14,37 @@ let db;
 
 mongoClient.connect().then(() => {
     db = mongoClient.db("batePapoUol");
+})
+
+app.post("/participants", async (req,res) => {
+    try{
+        let { name } = req.body;
+        let time = dayjs().format('HH:mm:ss');
+        if(!name || name.length === 0){
+            res.sendStatus(402);
+            return;
+        }
+
+         const already = await db.collection("users").findOne({name});
+
+        if(already){
+            res.status(409).send({erro: "Usuário já cadastrado!"});
+            return
+        }
+
+        await db.collection("users").insertOne({name, lastStatus: Date.now()});
+
+        await db.collection("messages").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: time});
+
+        res.sendStatus(201)   
+    }catch{
+        return res.sendStatus(422);
+    }
 })
 
 
